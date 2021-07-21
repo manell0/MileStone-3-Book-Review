1adcc12b22be74d0c237b4fda4da8438fa835f1d
manell0 <svante.magnell@gmail.com>
2021-07-21 12:35:09 +0000

    Add categories dynamic dropdown and template

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.collapsible').collapsible();
+    $('select').formSelect();
+    $(".datepicker").datepicker({
+      format: "yyyy",
+      yearRange: 100,
+      showClearBtn: true,
+      i18n: {
+          done: "Select"
+      }
+  });
   });
